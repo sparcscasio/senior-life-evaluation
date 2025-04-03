714a52028902b23267198ba568347f9f5f8a6b6f
sparcscasio <162997498+sparcscasio@users.noreply.github.com>
2025-04-04 02:08:43 +0900

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [vision, setVision] = useState(null);
-  const [auditory, setAuditory] = useState(null);
-  const [physical, setPhysical] = useState(null);
-  const [self_, setSelf] = useState(null);
-  const [communication, setCommunication] = useState(null);
+  const [vision, setVision] = useState(0);
+  const [auditory, setAuditory] = useState(0);
+  const [physical, setPhysical] = useState(0);
+  const [self_, setSelf] = useState(0);
+  const [emotional, setEmotional] = useState(0);
+  const [communication, setCommunication] = useState(0);
   const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState(null);
+  const [result, setResult] = useState(0);
 
-  const apiUrl = "https://706a-34-173-86-218.ngrok-free.app/calc";
+  const minRes = 0.4205267131328583;
+  const maxRes = 26.24397850036621;
+
+  const apiUrl = "https://4062-34-173-86-218.ngrok-free.app/calc";
 
   const handleCalculate = async () => {
     setLoading(true);
     try {
-      const response = await fetch(`${apiUrl}?vision=${vision}&auditory=${auditory}&physical=${physical}&self=${self_}&communication=${communication}`, {
+      const response = await fetch(`${apiUrl}?vision=${vision}&auditory=${auditory}&physical=${physical}&emotional=${emotional}&self=${self_}&communication=${communication}`, {
         headers: {
           'ngrok-skip-browser-warning': 'true'  // ngrok 경고 페이지를 생략
         }
@@ -26,7 +30,17 @@ const App = () => {
       // JSON 파싱 시도
       const data = JSON.parse(textResponse);
       console.log(data);
-      setResult(data.result);
+      const res = data.result;
+      const tuned = (res-minRes) / (maxRes-minRes);
+      if (tuned < 0) {
+        setResult(0);
+      } else {
+        if (tuned > 1) {
+          setResult(1);
+        } else {
+          setResult(tuned);
+        }
+      }
     } catch (error) {
       console.error("Error calling the API:", error);
     } finally {
@@ -44,7 +58,7 @@ const App = () => {
           <input 
             type="number" 
             value={vision} 
-            onChange={(e) => setVision(e.vision.value)} 
+            onChange={(e) => setVision(e.target.value)} 
           />
         </label>
         <label>
@@ -52,7 +66,7 @@ const App = () => {
           <input 
             type="number" 
             value={physical} 
-            onChange={(e) => setPhysical(e.physical.value)} 
+            onChange={(e) => setPhysical(e.target.value)} 
           />
         </label>
         <label>
@@ -60,7 +74,7 @@ const App = () => {
           <input 
             type="number" 
             value={auditory} 
-            onChange={(e) => setAuditory(e.auditory.value)} 
+            onChange={(e) => setAuditory(e.target.value)} 
           />
         </label>
         <label>
@@ -68,7 +82,15 @@ const App = () => {
           <input 
             type="number" 
             value={self_} 
-            onChange={(e) => setSelf(e.self_.value)} 
+            onChange={(e) => setSelf(e.target.value)} 
+          />
+        </label>
+        <label>
+          정서적 만족도: 
+          <input 
+            type="number" 
+            value={emotional} 
+            onChange={(e) => setEmotional(e.target.value)} 
           />
         </label>
         <label>
@@ -76,7 +98,7 @@ const App = () => {
           <input 
             type="number" 
             value={communication} 
-            onChange={(e) => setCommunication(e.communication.value)} 
+            onChange={(e) => setCommunication(e.target.value)} 
           />
         </label>
         <button onClick={handleCalculate} disabled={loading}>
